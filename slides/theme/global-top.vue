7dd3e7b93d827e2550158f6ed1c5838fe7591a9e
Gin5050 <38396388+Gin5050@users.noreply.github.com>
2025-06-13 23:39:21 +0900

    Add Slidev theme and layouts

---
+++ slides/theme/global-top.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="w-full py-2 px-6 flex items-center justify-between text-sm opacity-80">
+    <span class="font-semibold" style="color:var(--c-brand)">
+      {{ $slidev.configs.company ?? 'Your Company' }}
+    </span>
+    <span v-if="$slidev.nav.currentLayout !== 'cover'">
+      {{ $slidev.nav.currentPage?.title }}
+    </span>
+  </header>
+</template>
